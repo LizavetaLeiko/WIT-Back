1b3743e5ac9cad783c61927a1ce0469b4262391c
LizavetaLeiko <lizaleiko3@gmail.com>
2023-03-12 15:56:54 +0300

    fix: like and unlike route

--- router/index.js
+++ router/index.js
@@ -24,8 +24,8 @@ router.post('/createpost', postsController.createPost);
 router.get('/post/:id', postsController.getPost);
 router.get('/usersposts/:id', postsController.getAllUsersPosts);
 router.get('/posts', postsController.getAllPosts);
-router.put('/likepost', postsController.likePost);
-router.put('/unlikepost', postsController.unLikePost);
+router.put('/likepost/:id', postsController.likePost);
+router.put('/unlikepost/:id', postsController.unLikePost);
 
 
 module.exports = router
